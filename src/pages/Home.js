bbc2f30a93ae149597f22ef5f9e57e9279e6bcac
zereight <zereight@zereightui-MacBookPro.local>
2020-09-04 11:18:34 +0900

    init

---
+++ src/pages/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Home</h1>
+      <div className="home__header">
+        <div className="home__headerLeft">
+          {/* Link */}
+          {/* Link */}
+        </div>
+        <div className="home__headerRight">
+          {/* Link */}
+          {/* Link */}
+          {/* Icon */}
+          {/* Avatar */}
+        </div>
+      </div>
+      <div className="home__body"></div>
+    </div>
+  );
+};
+
+export default Home;
